0d655c689fdce846604ea2b79d3f0ca60b38545f
artu1000 <arthur.lorent@gmail.com>
2014-10-22 09:58:58 +0200

    dev ecran accueil

--- app/src/main/java/com/example/imie/navalwar/MyActivity.java
+++ app/src/main/java/com/example/imie/navalwar/MyActivity.java
@@ -1,9 +1,12 @@
 package com.example.imie.navalwar;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 
@@ -13,31 +16,24 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        Button btbegin= (Button) findViewById(R.id.btbegin);
+        btbegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent;
+                myIntent = new Intent(MyActivity.this, Board.class);
+                MyActivity.this.startActivity(myIntent);
+            }
+        });
+
+        Button btexit= (Button) findViewById(R.id.btexit);
+        btexit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    public void onClickCommencer(Button button){
-
-
-    }
 }
